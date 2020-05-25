44a8a41d4d91bf3ca63530c7389dd0cc42a633d0
Erickfpb <61366182+Erickfpb@users.noreply.github.com>
2020-05-25 12:24:35 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Components/index.css';
+import App from './Components/App';
+import * as serviceWorker from './serviceWorker';
+import 'tachyons'
+import {robots} from './Components/robots.js';
+
+ReactDOM.render(<App robots={robots}/>, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
