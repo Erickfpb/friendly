8f4ec6077d1725db49603121348a8a8010cdfa66
Erick Perez Barcenas <erick_fpb@hotmail.com>
2020-05-25 12:44:51 -0500

    Updates

---
+++ precache-manifest.cf315300cce72a938a7479ce7aa0c9c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3d09c80114758531105ec77f9561df1",
+    "url": "/friendly/index.html"
+  },
+  {
+    "revision": "e46389af8748e118cc9c",
+    "url": "/friendly/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "9c28acffd3eda2e1b86a",
+    "url": "/friendly/static/css/main.be00e998.chunk.css"
+  },
+  {
+    "revision": "e46389af8748e118cc9c",
+    "url": "/friendly/static/js/2.eeb447fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/friendly/static/js/2.eeb447fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c28acffd3eda2e1b86a",
+    "url": "/friendly/static/js/main.1bef2b55.chunk.js"
+  },
+  {
+    "revision": "d2b8a5feac0831431e2c",
+    "url": "/friendly/static/js/runtime-main.07b68515.js"
+  }
+]);
